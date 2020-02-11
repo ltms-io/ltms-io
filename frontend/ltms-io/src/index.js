746cf6ae16b555975dfa446d1d1c7f563f489754
Joseph Schmidt <schmi150@purdue.edu>
2020-02-11 15:07:31 -0500

    Create fields to display scores

--- frontend/ltms-io/src/index.js
+++ frontend/ltms-io/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Sheet from './Scoresheet';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Sheet />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
