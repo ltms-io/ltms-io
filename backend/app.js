7bab88ae3d9ce918b17a7778ebe6439d3c03f0e6
Jack Doherty <doherty9@purdue.edu>
2020-02-14 23:34:41 -0500

    tried debugging, did not work

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
+app.use("/api/users", usersRouter);
 app.use('/api/tournaments', tournamentsRouter);
 
 // catch 404 and forward to error handler
