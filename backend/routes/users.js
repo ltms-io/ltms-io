99e06dd43abceb0bfdf544402c00663feffeac49
Jack Doherty <doherty9@purdue.edu>
2020-02-14 19:03:21 -0500

    added acct change confirmation email. untested. currently no API calls are working

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user-model');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 /* GET */
 
@@ -37,6 +40,8 @@ router.post('/api/users', (req, res) => {
     //TODO: MORE PREP WORK FOR USER
 
     newUser.save().then(newUser => res.json(newUser)).catch(err => console.log(err));
+
+    //TODO: Send user confirmation email
 });
 
 /* PATCH */
@@ -46,35 +51,55 @@ router.post('/api/users', (req, res) => {
 //  -changes are supplied via body
 //  -if no change to a field, don't send it
 router.patch('/api/users/:id', (req, res) => {
+    if(Object.keys(req.body).length == 0) {
+        res.status(400).send("body is empty");
+        return;
+    }
+
     User.findById(req.params.id).then(user => {
-        if(!user) {
-            res.status(404).send("user not found");
-            return;
-        }
+        // if(!user) {
+        //     res.status(404).send("user not found");
+        //     return;
+        // }
+
+        summaryOfChanges = '';
 
         if(req.body.name) {
             user.name = req.body.name;
+            summaryOfChanges.concat(`•Name has been updated to ${req.body.name}\n`);
         }
 
         if(req.body.email) { //TODO add email validator
-            user.email = req.body.email;
+            // user.email = req.body.email;
+            summaryOfChanges.concat(`•Email has been updated to ${req.body.email}\n`); //TODO: maybe ask to confirm on old email if this is the case?
         }
 
         if(req.body.password) { //TODO: NOTE: adding this becaue it is in schema but I do not think we need this as Auth0 is managing passwords
             //TODO
+            summaryOfChanges.concat("•Password has been updated.\n");
         }
 
         if(req.body.eventAuthorizer) { //TODO: add authorization to this
             user.eventAuthorizer = req.body.eventAuthorizer;
+            summaryOfChanges.concat("•You have been authorized to create official events.\n");
         }
 
         if(req.body.userAuthorizer) { //TODO: add authorization to this
             user.userAuthorizer = req.body.userAuthorizer;
+            summaryOfChanges.concat("•You have been authorized to authorize other users to create official events.\n");
         }
 
         user.save();
 
         res.status(200).send("changes made successfully");
+
+        const msg = {
+            to: req.body.email,
+            from: 'noreply@ltmsio.codes',
+            subject: 'Changes have been made to your account',
+            text: msg,
+            html: msg,
+        };
     })
 });
 
