1148c46b845416a3e8c9a296f1c23f18150e213f
neelc17 <neelch1154@gmail.com>
2020-02-14 23:09:00 -0500

    set up basic Redux store state change functionality

--- frontend/ltms-io/src/index.js
+++ frontend/ltms-io/src/index.js
@@ -5,6 +5,30 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { createStore } from 'redux';
+
+function reducer(state, action) {
+	if (action.type == 'changeState') {
+		return action.payload.newState;
+	}
+
+	return 'State';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+	type: 'changeState',
+	payload: {
+		newState: 'New state'
+	}
+};
+
+store.dispatch(action);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
