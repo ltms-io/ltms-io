0eff42a4c0a656edccdb096d2507011bf47027f2
neelc17 <neelch1154@gmail.com>
2020-02-18 17:11:29 -0500

    added basic login submit handling functionality

--- frontend/ltms-io/src/Login.js
+++ frontend/ltms-io/src/Login.js
@@ -3,26 +3,25 @@ import { connect } from 'react-redux';
 import { Form, Button } from 'react-bootstrap'
 
 class Login extends Component {
-  render () {
+  handleSubmit(e) {
+    e.preventDefault();
+    alert("Email: " + e.target.elements.email.value + "\nPassword: " + e.target.elements.password.value);
+  }
+
+  render() {
     return(
       <div>
         <h1>Login</h1>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="email">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email address" />
-            <Form.Text className="text-muted">
-              We'll share your email and SSN with everyone.
-            </Form.Text>
           </Form.Group>
           <Form.Group controlId="password">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Enter password" />
-            <Form.Text className="text-muted">
-              We'll share your email and SSN with everyone.
-            </Form.Text>
           </Form.Group>
-          <Button variant="outline-primary" type="">
+          <Button variant="outline-primary" type="submit">
             Log In
           </Button>
         </Form>
