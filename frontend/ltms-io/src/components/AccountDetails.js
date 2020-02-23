cf74098fe665a932674ec65b932497a8afbcb720
neelc17 <neelch1154@gmail.com>
2020-02-23 03:05:43 -0500

    added basic navbar component

--- frontend/ltms-io/src/components/AccountDetails.js
+++ frontend/ltms-io/src/components/AccountDetails.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Form, Button, Container, Row, Col, Image } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import ResetLogin from './ResetLogin';
 import logo from '../logo.svg';
 
 class AccountDetails extends Component {
   constructor(props) {
     super(props);
-    
+
     this.handleName = this.handleName.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
   }
@@ -27,42 +27,46 @@ class AccountDetails extends Component {
     return(
       <div>
         <h1>Edit Account Details</h1>
-        <Container>
-          <Row>
-            <Col>
-              <div>
-                <h3>Edit Profile Picture</h3>
-                <img src={logo} />
-              </div>
-              <div>
-                <h3>Edit Name</h3>
-                <Form onSubmit={this.handleName}>
-                  <Row>
-                    <Col>
-                      <Form.Group controlId="firstname">
-                        <Form.Control />
-                      </Form.Group>
-                    </Col>
-                    <Col>
-                      <Form.Group controlId="lastname">
-                        <Form.Control />
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  <Button variant="outline-primary" type="submit">
-                    Submit
-                  </Button>
-                </Form>
-              </div>
-            </Col>
-            <Col>
-              <ResetLogin />
-            </Col>
-          </Row>
-        </Container>
-        <Button variant="outline-danger" onClick={this.handleDelete}>
-          Delete my Account
-        </Button>
+        <div>
+          <Container>
+            <Row>
+              <Col>
+                <div>
+                  <h3>Edit Profile Picture</h3>
+                  <img src={logo} />
+                </div>
+                <div>
+                  <h3>Edit Name</h3>
+                  <Form onSubmit={this.handleName}>
+                    <Row>
+                      <Col>
+                        <Form.Group controlId="firstname">
+                          <Form.Control />
+                        </Form.Group>
+                      </Col>
+                      <Col>
+                        <Form.Group controlId="lastname">
+                          <Form.Control />
+                        </Form.Group>
+                      </Col>
+                    </Row>
+                    <Button variant="outline-primary" type="submit">
+                      Submit
+                    </Button>
+                  </Form>
+                </div>
+              </Col>
+              <Col>
+                <ResetLogin />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div>
+          <Button variant="outline-danger" onClick={this.handleDelete}>
+            Delete my Account
+          </Button>
+        </div>
       </div>
     );
   }
