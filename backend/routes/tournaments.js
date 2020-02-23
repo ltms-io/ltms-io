c7c886919a011f3319628a2508f64a3b02e93cdd
Hank Krutulis <henrykrutulis@gmail.com>
2020-02-23 12:34:15 -0500

    Added tournament update route and tournament registration

--- backend/routes/tournaments.js
+++ backend/routes/tournaments.js
@@ -16,12 +16,30 @@ router.get('/:id', (req, res) => {
     }
     
     res.send(exampleTournament);
-})
+});
 
 /* GET tournaments by users */
 router.get('/user/:id', (req, res) => {
     
-})
+});
+
+/* POST register new tournament */
+router.post('/register', (req, res) => {
+    if (!req.body.director || !req.body.fieldsCount) {
+        return res.status(400).json(errors);
+    }
+
+    if (false) { //TODO: Add a check for recent tournament created by user
+        return res.status(400).send("Tournament recently ceated");
+    }
+    else {
+        const createdTournament = new User({
+            director: req.body.name,
+            fieldsCount: req.body.fieldsCount,
+        });
+        createdTournament.save().then((tournament) => res.send(tournament)).catch((err) => console.log(err));
+    }
+});
 
 /* UPDATE specific tournament. */
 router.post('/:id', (req, res) => {
@@ -31,6 +49,27 @@ router.post('/:id', (req, res) => {
     }
     
     //TODO: Update tournament based upon req.body.tournament
-})
+    Tournament.findById(req.body.id).then((tournament) => {
+        if (req.body.director) {
+            tournament.director = req.body.director;
+        }
+
+        if (req.body.name) {
+            tournament.naem = req.body.name;
+        }
+
+        if (req.body.teams) {
+            tournament.teams = req.body.teams;
+        }
+
+        if (req.body.fieldsCount) {
+            tournament.fieldsCount = req.body.fieldsCount;
+        }
+
+        if (req.body.matchesPerTeam) {
+            tournament.matchesPerTeam = req.body.matchesPerTeam;
+        }
+    });
+});
 
 module.exports = router;
