fb7e430213addf9180b053b60ac6d9e058c68691
Hank Krutulis <henrykrutulis@gmail.com>
2020-02-25 20:48:23 -0500

    Added cors as middleware for API

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -20,6 +21,9 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+//CORS Middleware to enable api calls from same origin
+app.use(cors());
 
 //DB config
 const db = "mongodb+srv://FLTMS_App:pcXQ4HvRcyASuxeq@clusterfltms-yea2u.azure.mongodb.net/test?retryWrites=true&w=majority"
