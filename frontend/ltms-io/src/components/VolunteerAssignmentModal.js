8652cf67bdf2bd1dc0fd5a2f029759f72d750ab2
Hank Krutulis <henrykrutulis@gmail.com>
2020-02-27 15:14:27 -0500

    Got access to field text for email lookup

--- frontend/ltms-io/src/components/VolunteerAssignmentModal.js
+++ frontend/ltms-io/src/components/VolunteerAssignmentModal.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
-import { Container, Form, Button, Modal, Row, Col } from 'react-bootstrap'
+import axios from 'axios';
+import { Container, Form, FormControl, Button, Modal, Row, Col } from 'react-bootstrap'
 
 
 export default class VolunteerAssignmentModal extends Component {
     constructor(props) {
         super(props)
+        this.email = '';
+        this.emailInput = React.createRef();
+        this.searchFunction = this.searchFunction.bind(this);
     }
+
+    searchFunction() {
+        const params = {
+            email: this.emailInput.current.value,
+        }
+    
+        //axios.get({url: 'http://localhost:5000/api/users/email', data: params}).then((x) => {console.log(x)});
+        console.log(this.emailInput.current.value);
+    }
+
 
     render() {
         return (
             <Container>
                 <Modal
-                show={true || this.props.show}
+                show={this.props.show}
                 onHide={this.props.handleClose}
                 size="lg"
                 aria-labelledby="contained-modal-title-vcenter"
@@ -23,22 +37,28 @@ export default class VolunteerAssignmentModal extends Component {
                     </Modal.Header>
                     <Modal.Body>
                         <Row>
-                            <Form>
-                                <Form.Group as={Row} controlId="volunteerSearchBox">
-                                    <Form.Label column md="6">Input Volunteer Email</Form.Label>
-                                    <Col lg="10">
-                                        <Form.Control type="email" placeholder="Enter Email"/>
-                                    </Col>
-                                    <Button column type="submit">Search</Button>
-                                </Form.Group>
-                            </Form>
+                            <Col>
+                                <Form>
+                                    <Form.Group as={Row} controlId="volunteerSearchBox">
+                                        <Form.Label  md="6">Input Volunteer Email</Form.Label>
+                                        <Col lg="10">
+                                            <FormControl 
+                                                type="email"
+                                                placeholder="User Email"
+                                                ref={this.emailInput}/>
+                                        </Col>
+                                        <Button column='1' type="button" color="primary"  onClick={this.searchFunction}>Search</Button>
+                                    </Form.Group>
+                                </Form>
+                            </Col>
                         </Row>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button onClick="{this.props.show = false}">Close</Button>
+                        <Button onClick={this.props.handleClose}>Close</Button>
                     </Modal.Footer>
                 </Modal>
             </Container>
         )
     }
+
 }
