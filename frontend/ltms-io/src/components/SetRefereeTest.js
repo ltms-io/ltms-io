c4b927b85f2daecf6eb5e808336b734f78e85ec7
neelc17 <neelch1154@gmail.com>
2020-03-13 14:14:32 -0700

    set referee test succesfully patches tournament with new referee volunteers

--- frontend/ltms-io/src/components/SetRefereeTest.js
+++ frontend/ltms-io/src/components/SetRefereeTest.js
@@ -9,41 +9,15 @@ class SetRefereeTest extends Component {
 
     this.state = {
       dbresults: {},
+      dbtournresults: {},
       authresults: {}
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateState = this.updateState.bind(this);
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    alert(e.target.elements.users.value);
-    var strings = e.target.elements.users.value.split(",");
-    for (var i = 0; i < strings.length; i++) {
-      strings[i] = strings[i].trim();
-    }
-    console.log(strings);
-  }
-
-  render() {
-    return(
-      <div>
-        <h1>Set Referee Test</h1>
-        <div>
-          <h3>Reset Email Address</h3>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Group controlId="users">
-              <Form.Label>Enter user(s) below (separated by commas)</Form.Label>
-              <Form.Control type="text" placeholder="Enter user(s)" />
-            </Form.Group>
-            <Button variant="outline-primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </div>
-    );
-  }
-
-  async componentDidMount() {
+  async updateState() {
     await axios({
       method: 'GET',
       url: `https://dev-s68c-q-y.auth0.com/userinfo`,
@@ -68,16 +42,68 @@ class SetRefereeTest extends Component {
         console.log(error);
     });
 
+    await axios.get(`http://localhost:5000/api/tournaments/5e654dab2050893113969494`)
+    .then ( (result) => {
+        this.state.dbtournresults = result.data;
+    }).catch( (error) => {
+        console.log(error);
+    });
+
+    this.setState(this.state);
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    alert(e.target.elements.users.value);
+    var strings = e.target.elements.users.value.split(",");
+    var newUsers = [];
+    for (var i = 0; i < strings.length; i++) {
+      strings[i] = strings[i].trim();
+      newUsers = [
+        ...newUsers,
+        {
+          user: strings[i],
+          uRole: "referee"
+        }
+      ];
+    }
+    await axios.patch(`http://localhost:5000/api/tournaments/5e654dab2050893113969494`, {
+      volunteers: [
+        ...this.state.dbtournresults.volunteers,
+        ...newUsers
+      ]
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
+
+    this.updateState();
+    console.log("UPDATED STATE", this.state);
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Set Referee Test</h1>
+        <div>
+          <h3>Reset Email Address</h3>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="users">
+              <Form.Label>Enter user(s) below (separated by commas)</Form.Label>
+              <Form.Control type="text" placeholder="Enter user(s)" />
+            </Form.Group>
+            <Button variant="outline-primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.updateState();
     console.log("INITIAL SET REFEREE TEST STATE", this.state);
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-    name: state.name,
-    email: state.email,
-    tournaments: state.tournaments
-  }
-};
-
-export default connect(mapStateToProps)(SetRefereeTest);
+export default SetRefereeTest;
