0f472869be077ffc7fc245f02f3557e8ceb8a9d6
singh497 <47434320+singh497@users.noreply.github.com>
2020-03-13 19:29:49 -0400

    Edit Tournament Form

--- frontend/ltms-io/src/components/EditTournament.js
+++ frontend/ltms-io/src/components/EditTournament.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Form, Button, Col, Row } from "react-bootstrap";
 
 export default class EditTournament extends Component {
   render() {
-    return <div>Edit Tournament Page</div>;
+    return (
+      <div>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
