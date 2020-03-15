03118b009a177a07b4b30b703d8e7c43523e45a5
singh497 <47434320+singh497@users.noreply.github.com>
2020-03-15 12:51:50 -0400

    Update forms

--- frontend/ltms-io/src/components/EditTournament.js
+++ frontend/ltms-io/src/components/EditTournament.js
@@ -7,20 +7,17 @@ export default class EditTournament extends Component {
     return (
       <div>
         <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+          <Form.Group controlId="exampleForm.ID">
+            <Form.Label>Enter Tournament ID</Form.Label>
+            <Form.Control as="textarea" rows="1" />
           </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group controlId="exampleForm.TeamNum">
+            <Form.Label>Enter Team Number</Form.Label>
+            <Form.Control as="textarea" rows="1" />
           </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+          <Form.Group controlId="exampleForm.TeamName">
+            <Form.Label>Enter Team Name</Form.Label>
+            <Form.Control as="textarea" rows="1" />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
