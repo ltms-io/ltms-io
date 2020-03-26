28a135f4737a40011ec08d4eb1483eaf1a655a0c
Joseph Schmidt <schmi150@purdue.edu>
2020-03-26 13:35:03 -0400

    Dynamic adding in the works

--- frontend/ltms-io/src/components/Scoresheet.js
+++ frontend/ltms-io/src/components/Scoresheet.js
@@ -20,195 +20,240 @@ class Sheet extends React.Component{
   //function to handle the calculate score call
   handleInsert(e) {
     e.preventDefault();
+    var newArray = [...this.state.events];
+    newArray.push({
+      categ: e.target.elements.category.value,
+      scoretype: e.target.elements.score.value
+    })
+    this.setState({events: newArray})
+  //   e.preventDefault();
 
-    //new score to hold the final score
-    var newScore = 0;
+  //   //new score to hold the final score
+  //   var newScore = 0;
     
-    //begins adding all scores to the newScore variable
-    if(parseInt(e.target.elements.scoring1.value, 10)){
-      newScore += parseInt(e.target.elements.scoring1.value, 10);
-    }
+  //   //begins adding all scores to the newScore variable
+  //   if(parseInt(e.target.elements.scoring1.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring1.value, 10);
+  //   }
     
-    if(parseInt(e.target.elements.scoring2.value, 10)){
-      newScore += parseInt(e.target.elements.scoring2.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring2.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring2.value, 10);
+  //   }
 
-    if(parseInt(e.target.elements.scoring3.value, 10)){
-      newScore += parseInt(e.target.elements.scoring3.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring3.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring3.value, 10);
+  //   }
     
-    if(parseInt(e.target.elements.scoring4.value, 10)){
-      newScore += parseInt(e.target.elements.scoring4.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring4.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring4.value, 10);
+  //   }
 
-    if(parseInt(e.target.elements.scoring5.value, 10)){
-      newScore += parseInt(e.target.elements.scoring5.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring5.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring5.value, 10);
+  //   }
 
-    if(parseInt(e.target.elements.scoring6.value, 10)){
-      newScore += parseInt(e.target.elements.scoring6.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring6.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring6.value, 10);
+  //   }
 
-    if(parseInt(e.target.elements.scoring7.value, 10)){
-      newScore += parseInt(e.target.elements.scoring7.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring7.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring7.value, 10);
+  //   }
 
-    if(parseInt(e.target.elements.scoring8.value, 10)){
-      newScore += parseInt(e.target.elements.scoring8.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring8.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring8.value, 10);
+  //   }
 
-    if(parseInt(e.target.elements.scoring9.value, 10)){
-      newScore += parseInt(e.target.elements.scoring9.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring9.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring9.value, 10);
+  //   }
 
-    if(parseInt(e.target.elements.scoring10.value, 10)){
-      newScore += parseInt(e.target.elements.scoring10.value, 10);
-    }
+  //   if(parseInt(e.target.elements.scoring10.value, 10)){
+  //     newScore += parseInt(e.target.elements.scoring10.value, 10);
+  //   }
 
-    this.setState({finalscore: newScore});
+  //   this.setState({finalscore: newScore});
 
   }
     
   render(){
+
     return(
-      //script to render the page
       <div>
-        <h3>Team Scoring</h3>
-        <Form onSubmit = {this.handleInsert}>
+        <h3>Add a category</h3>
+        <Form onSubmit={this.handleInsert}>
           <Row>
             <Col>
-              <Form.Group controlId = "category1">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring1">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category2">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring2">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category3">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring3">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category4">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring4">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category5">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring5">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category6">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring6">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category7">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring7">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category8">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring8">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category9">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring9">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId = "category10">
-                <Form.Control type = "text" placeholder = "Category"/>
-              </Form.Group>
-            </Col>
-            <Col xs = "3">
-              <Form.Group controlId = "scoring10">
-                <Form.Control type = "text" placeholder = "Score"/>
-              </Form.Group>
-            </Col>
+          <Form.Group controlId = "category">
+            <Form.Control type = "text" placeholder = "Category"/>
+          </Form.Group>
+          </Col>
+          <Col>
+          <Form.Group controlId = "score">
+            <Form.Control type = "text" placeholder = "Score Type"/>
+          </Form.Group>
+          </Col>
           </Row>
           <Button variant = "outline-primary" type = "submit">
-             Calculate Score
+            Add Category
           </Button>
         </Form>
         <Form>
-          <Row>
-            <Col xs = "1">Final Score</Col>
-            <Col xs = "2">
-              <Form.Control type = "text" placeholder = {this.state.finalscore} readonly = "true"/>
-            </Col>
-          </Row>
+          {this.state.events.map(event => (
+            <Cate
+            categ = {event.categ}
+            />
+          ))}
         </Form>
       </div>
-    ); 
+    );
+    // return(
+    //   //script to render the page
+    //   <div>
+    //     <h3>Team Scoring</h3>
+    //     <Form onSubmit = {this.handleInsert}>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category1">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring1">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category2">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring2">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category3">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring3">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category4">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring4">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category5">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring5">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category6">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring6">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category7">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring7">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category8">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring8">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category9">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring9">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <Form.Group controlId = "category10">
+    //             <Form.Control type = "text" placeholder = "Category"/>
+    //           </Form.Group>
+    //         </Col>
+    //         <Col xs = "3">
+    //           <Form.Group controlId = "scoring10">
+    //             <Form.Control type = "text" placeholder = "Score"/>
+    //           </Form.Group>
+    //         </Col>
+    //       </Row>
+    //       <Button variant = "outline-primary" type = "submit">
+    //          Calculate Score
+    //       </Button>
+    //     </Form>
+    //     <Form>
+    //       <Row>
+    //         <Col xs = "1">Final Score</Col>
+    //         <Col xs = "2">
+    //           <Form.Control type = "text" placeholder = {this.state.finalscore} readonly = "true"/>
+    //         </Col>
+    //       </Row>
+    //     </Form>
+    //   </div>
+    // ); 
+  }
+}
+
+class Cate extends Component{
+  render(){
+    return(
+      {this.props.events}
+    );
   }
 }
 
