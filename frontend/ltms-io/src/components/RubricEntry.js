628c8b646de1ca6d254978d880db38b5a48973e9
Neel Chaudhari <neelch1154@gmail.com>
2020-03-28 12:51:59 -0700

    added basic validation for rubric form

--- frontend/ltms-io/src/components/RubricEntry.js
+++ frontend/ltms-io/src/components/RubricEntry.js
@@ -65,6 +65,8 @@ class RubricEntry extends Component {
 
   async handleSubmit(e) {
     e.preventDefault();
+    alert("Submitted!");
+    console.log(e.target.elements);
   }
 
   render() {
@@ -72,7 +74,7 @@ class RubricEntry extends Component {
       <div>
         <h1>Rubric Entry for {this.state.dbteamresults.teamName} in {this.state.dbtournresults.name}</h1>
         <div>
-          <Form>
+          <Form onSubmit={this.handleSubmit}>
             <div>
               <h3>Core Values</h3>
               <Container>
@@ -80,8 +82,8 @@ class RubricEntry extends Component {
                   <h6>Inspiration</h6>
                   <Form.Group as="Col" controlId="formDiscovery">
                     <Form.Label>Discovery</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -90,8 +92,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formTeamIdentity">
                     <Form.Label>Team Identity</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required  as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -100,8 +102,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formImpact">
                     <Form.Label>Impact</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -113,8 +115,8 @@ class RubricEntry extends Component {
                   <h6>Teamwork</h6>
                   <Form.Group as="Col" controlId="formEfficiency">
                     <Form.Label>Effectiveness</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -123,8 +125,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formEfficiency">
                     <Form.Label>Efficiency</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -133,8 +135,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formKidsDoTheWork">
                     <Form.Label>Kids Do the Work</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -146,8 +148,8 @@ class RubricEntry extends Component {
                   <h6>Gracious Professionalism®</h6>
                   <Form.Group as="Col" controlId="formInclusion">
                     <Form.Label>Inclusion</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -156,8 +158,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formRespect">
                     <Form.Label>Respect</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -166,8 +168,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formCoopertition">
                     <Form.Label>Coopertition®</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -188,8 +190,8 @@ class RubricEntry extends Component {
                   <h6>Research</h6>
                   <Form.Group as="Col" controlId="formProblemIdentification">
                     <Form.Label>Problem Identification</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -198,8 +200,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formSourcesOfInformation">
                     <Form.Label>Sources of Information</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -208,8 +210,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formProblemAnalysis">
                     <Form.Label>Problem Analysis</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -221,8 +223,8 @@ class RubricEntry extends Component {
                   <h6>Innovative Solution</h6>
                   <Form.Group as="Col" controlId="formTeamSolution">
                     <Form.Label>Team Solution</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -231,8 +233,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formInnovation">
                     <Form.Label>Innovation</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -241,8 +243,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formSolutionDevelopment">
                     <Form.Label>Solution Development</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -254,8 +256,8 @@ class RubricEntry extends Component {
                   <h6>Presentation</h6>
                   <Form.Group as="Col" controlId="formSharing">
                     <Form.Label>Sharing</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -264,8 +266,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formCreativity">
                     <Form.Label>Creativity</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -274,8 +276,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formPresentationEffectiveness">
                     <Form.Label>Presentation Effectiveness</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -296,8 +298,8 @@ class RubricEntry extends Component {
                   <h6>Mechanical Design</h6>
                   <Form.Group as="Col" controlId="formDurability">
                     <Form.Label>Durability</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -306,8 +308,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formMechanicalEfficiency">
                     <Form.Label>Mechanical Efficiency</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -316,8 +318,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formMechanization">
                     <Form.Label>Mechanization</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -329,8 +331,8 @@ class RubricEntry extends Component {
                   <h6>Programming</h6>
                   <Form.Group as="Col" controlId="formProgrammingQuality">
                     <Form.Label>Programming Quality</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -339,8 +341,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formProgrammingEfficiency">
                     <Form.Label>Programming Efficiency</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -349,8 +351,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formAutomationNavigation">
                     <Form.Label>Automation/Navigation</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -362,8 +364,8 @@ class RubricEntry extends Component {
                   <h6>Strategy & Innovation</h6>
                   <Form.Group as="Col" controlId="formDesignProcess">
                     <Form.Label>Design Process</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -372,8 +374,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formMissionStrategy">
                     <Form.Label>Mission Strategy</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
@@ -382,8 +384,8 @@ class RubricEntry extends Component {
                   </Form.Group>
                   <Form.Group as="Col" controlId="formInnovation">
                     <Form.Label>Innovation</Form.Label>
-                    <Form.Control as="select" value="Choose...">
-                      <option>Choose...</option>
+                    <Form.Control required as="select">
+                      <option></option>
                       <option>Beginning</option>
                       <option>Developing</option>
                       <option>Accomplished</option>
