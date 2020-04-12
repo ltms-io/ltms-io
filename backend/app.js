ee3cea9eb607a9f850a29bf046f5957d2c900adc
Jack Doherty <doherty9@purdue.edu>
2020-04-11 20:23:12 -0400

    debug

--- backend/app.js
+++ backend/app.js
@@ -55,6 +55,8 @@ app.use('/api/tournaments', tournamentsRouter);
 app.use('/api/tournament', tournamentsRouter);
 app.use('/api/teams', teamsRouter);
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
 if(process.env.NODE_ENV == "production") {
   app.use("/", express.static("./frontend")); //START HERE: I need to figure out how to readjust calls
 }
