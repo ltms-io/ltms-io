59211d7d6f0bcddab787e91c26f555316986de72
Jack Doherty <doherty9@purdue.edu>
2020-04-11 20:59:10 -0400

    index router removed

--- backend/app.js
+++ backend/app.js
@@ -54,7 +54,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/user', usersRouter);
 app.use('/api/tournaments', tournamentsRouter);
