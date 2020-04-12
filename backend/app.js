aaa5dc2be201ccda8cf432378e2741b60ae889b9
Jack Doherty <doherty9@purdue.edu>
2020-04-11 21:46:40 -0400

    debug?

--- backend/app.js
+++ backend/app.js
@@ -37,16 +37,6 @@ mongoose
   .then(() => console.log("MongoDB successfully connected\n"))
   .catch(err => console.log(err));
 
-if (process.env.NODE_ENV == "production") {
-  console.log("yeet")
-  app.use("/", express.static("./frontend"));
-} else {
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
-  app.use(express.static(path.join(__dirname, 'public')));
-}
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -58,6 +48,16 @@ app.use('/api/tournaments', tournamentsRouter);
 app.use('/api/tournament', tournamentsRouter);
 app.use('/api/teams', teamsRouter);
 
+if (process.env.NODE_ENV == "production") {
+  const publicPath = path.join(__dirname, '../frontend');
+  app.use(express.static(publicPath));
+  app.use('*', express.static(publicPath));
+} else {
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 
 // // catch 404 and forward to error handler
 // app.use(function (req, res, next) {
