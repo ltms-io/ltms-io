a4dca1017053220b372e8012714c4bd61cb2faa8
singh497 <47434320+singh497@users.noreply.github.com>
2020-04-19 19:02:22 -0400

    Timer fixes

--- frontend/ltms-io/src/components/Timer.js
+++ frontend/ltms-io/src/components/Timer.js
@@ -11,7 +11,7 @@ export default class Timer extends Component {
 
     this.state = {
       counter: 0,
-      initalCounter: 0,
+      initalCounter: 150000,
       start: 0,
       go: false
     };
@@ -19,15 +19,17 @@ export default class Timer extends Component {
     this.stopTimer = this.stopTimer.bind(this)
     this.resetTimer = this.resetTimer.bind(this)
   }
-  
+  componentDidMount() {
+    this.setState({counter: 150000})
+  }
   startTimer(){
     this.setState({
       counter: this.state.counter,
-      start: Date.now() - this.state.counter,
+      start: Date.now() + this.state.counter,
       go: true
     })
     this.timer = setInterval(() => this.setState({
-      counter: Date.now() - this.state.start
+      counter: this.state.start - Date.now()
     }), 1);
   }
   stopTimer() {
@@ -35,16 +37,16 @@ export default class Timer extends Component {
     clearInterval(this.timer)
   }
   resetTimer() {
-    this.setState({counter: 0})
+    this.setState({counter: this.state.initalCounter})
   }
   render() {
     return (
     <div>
         <h3>Timer: {ms(this.state.counter)}</h3>
-        <button disabled={!this.state.counter == 0} onClick={this.startTimer}>start</button>
-        <button disabled={!this.state.go} onClick={this.stopTimer}>stop</button>
-        <button disabled={!(this.state.coutner != 0 && !this.state.go)} onClick={this.resetTimer}>reset</button>
-        <button disabled={!(this.state.coutner != 0 && !this.state.go)} onClick={this.startTimer}>resume</button>
+        <button disabled = {!(this.state.counter == this.state.initalCounter)} onClick={this.startTimer}>start</button>
+        <button disabled = {!this.state.go} onClick={this.stopTimer}>stop</button>
+        <button disabled={!(!(this.state.counter == this.state.initalCounter) && !this.state.go)} onClick={this.resetTimer}>reset</button>
+        <button disabled={!(!(this.state.counter == this.state.initalCounter) && !this.state.go)} onClick={this.startTimer}>resume</button>
     </div>
     );
   }
