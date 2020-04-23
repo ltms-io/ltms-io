394f00a7b975497846790b0d7abe791bb649c4bc
Neel Chaudhari <neelch1154@gmail.com>
2020-04-23 01:06:46 -0700

    updated QuickLinks

--- frontend/ltms-io/src/components/QuickLinks.js
+++ frontend/ltms-io/src/components/QuickLinks.js
@@ -12,30 +12,31 @@ class QuickLinks extends Component {
     };
   }
 
-  render() {
-    return(
-      <div>
-        <h1>Quick Links</h1>
-        <ListGroup>
-          <ListGroup.Item><a target="_blank" rel="noopener noreferrer" href="https://www.firstinspires.org/robotics/fll">FLL Homepage</a></ListGroup.Item>
-          <ListGroup.Item><a target="_blank" rel="noopener noreferrer" href="https://www.firstinspires.org/robotics/fll/challenge-and-season-info">FLL Challenge & Season Info</a></ListGroup.Item>
-          <ListGroup.Item><a target="_blank" rel="noopener noreferrer" href="https://www.firstinspires.org/resource-library?flagged=All&combine=&field_content_type_value%5B%5D=first_lego_league&field_resource_library_tags_tid=All&field_resource_library_tags_tid=All&sort_by=created_1">FLL Resource Library</a></ListGroup.Item>
-        </ListGroup>
-      </div>
-    );
-  }
-
   async componentDidMount() {
     if (document.cookie.length) {
       var token = document.cookie.substring(13);
       var decoded = jsonWeb.verify(token, "123456");
 
-      this.state.dbresults = decoded;
-      this.state.uid = decoded.auth0id;
+      await this.setState({
+        dbresults: decoded,
+        uid: decoded.auth0id
+      });
     }
-    this.setState(this.state);
 
-    console.log("INITIAL QUICKLINKS STATE", this.state);
+    console.log("INITIAL QUICK LINKS STATE", this.state);
+  }
+
+  render() {
+    return(
+      <div className="pl-3 pr-3 pt-2">
+        <h1>Quick Links</h1>
+        <ListGroup>
+          <ListGroup.Item action target="_blank" rel="noopener noreferrer" href="https://www.firstinspires.org/robotics/fll">FLL Homepage</ListGroup.Item>
+          <ListGroup.Item action target="_blank" rel="noopener noreferrer" href="https://www.firstinspires.org/robotics/fll/challenge-and-season-info">FLL Challenge & Season Info</ListGroup.Item>
+          <ListGroup.Item action target="_blank" rel="noopener noreferrer" href="https://www.firstinspires.org/resource-library?flagged=All&combine=&field_content_type_value%5B%5D=first_lego_league&field_resource_library_tags_tid=All&field_resource_library_tags_tid=All&sort_by=created_1">FLL Resource Library</ListGroup.Item>
+        </ListGroup>
+      </div>
+    );
   }
 }
 
