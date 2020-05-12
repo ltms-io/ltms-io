ca197b19b9aed4f5d8ba23462e5a6de6eedd9220
Jack Doherty <doherty9@purdue.edu>
2020-05-11 22:04:36 -0400

    removed jade, security risk

--- backend/app.js
+++ backend/app.js
@@ -52,11 +52,6 @@ if (process.env.NODE_ENV == "production") {
   const publicPath = path.join(__dirname, './frontend');
   app.use(express.static(publicPath));
   app.use('*', express.static(publicPath));
-} else {
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
-  app.use(express.static(path.join(__dirname, 'public')));
 }
 
 // // catch 404 and forward to error handler
